93724f5b8b3c3a6ba02f09af4cbcd1ea0268128a
MahmoudAbdulfattah1 <93574572+MahmoudAbdulfattah1@users.noreply.github.com>
2025-07-10 17:43:01 +0300

    feat: add Role enum and enhance User entity with validation and timestamps.

--- src/main/java/com/cineverse/cineverse/domain/entity/User.java
+++ src/main/java/com/cineverse/cineverse/domain/entity/User.java
@@ -1,10 +1,15 @@
 package com.cineverse.cineverse.domain.entity;
 
+import com.cineverse.cineverse.domain.enums.Role;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Set;
 
 @Entity
@@ -12,24 +17,50 @@ import java.util.Set;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Table(name = "APP_USER")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Email
+    @NotBlank
+    @Column(unique = true)
     private String email;
+    @NotBlank
     @Column(unique = true)
     private String username;
+    @NotBlank
+    @Size(min = 8, max = 20)
     private String password;
     private String name;
     @Column(name = "date_of_birth")
     private LocalDate dateOfBirth;
-    @Column(name = "profile_picture")
-    private String profilePicture;
+    private String profilePictureUuid;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+    private boolean enabled;
     @JsonIgnore
     @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
     private Set<Review> reviews;
     @JsonIgnore
     @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
     private Set<Watchlist> watchlists;
+
+    @PrePersist
+    protected void onCreate() {
+        enabled = false;
+        role = Role.USER;
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
 }
---
+++ src/main/java/com/cineverse/cineverse/domain/enums/Role.java
@@ -0,0 +1,6 @@
+package com.cineverse.cineverse.domain.enums;
+
+public enum Role {
+    USER,
+    ADMIN
+}
