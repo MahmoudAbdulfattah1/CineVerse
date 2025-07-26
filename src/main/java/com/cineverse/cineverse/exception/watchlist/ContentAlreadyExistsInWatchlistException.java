149c5b38babbd460a305b745bf6f447b3e12f889
MahmoudAbdulfattah1 <93574572+MahmoudAbdulfattah1@users.noreply.github.com>
2025-07-26 19:45:21 +0300

    feature: Add custom exceptions for watchlist handling.

---
+++ src/main/java/com/cineverse/cineverse/exception/watchlist/ContentAlreadyExistsInWatchlistException.java
@@ -0,0 +1,7 @@
+package com.cineverse.cineverse.exception.watchlist;
+
+public class ContentAlreadyExistsInWatchlistException extends RuntimeException {
+    public ContentAlreadyExistsInWatchlistException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/cineverse/cineverse/exception/watchlist/WatchlistNotFoundException.java
@@ -0,0 +1,7 @@
+package com.cineverse.cineverse.exception.watchlist;
+
+public class WatchlistNotFoundException extends RuntimeException {
+    public WatchlistNotFoundException(String message) {
+        super(message);
+    }
+}
